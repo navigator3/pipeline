ee2d086282587dee8a5306b6811dcbcf5b87a3c7
sshevtsov <sergei_shevtsov@epam.com>
2020-11-02 20:55:58 +0300

    pipeline

--- install/checkfile_tmp.sh
+++ install/checkfile_tmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-V_NUMBER=${BUILD_NUMBER}
-CURRENT_VER=\$(cat /usr/local/tomcat/webapps/helloworld-ws/index.html | grep ver | awk -F "ver:" {'print \$2'} | awk -F "<" {'print \$1'} | sed '/^$/d')
-if [ "\$CURRENT_VER" -eq "\$V_NUMBER" ]; then
+V_NUMBER=BUILD_NUMBER
+CURRENT_VER=$(cat /usr/local/tomcat/webapps/helloworld-ws/index.html | grep ver | awk -F "ver:" {'print $2'} | awk -F "<" {'print $1'} | sed '/^$/d')
+if [ "$CURRENT_VER" -eq "$V_NUMBER" ]; then
   exit 0
 else
   exit 1
